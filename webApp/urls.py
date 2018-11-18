856ef51e9e0165bcf9bd6646f8aaa9e9214097c0
carlos.andres.sierra.v@gmail.com <casierrav@unal.edu.co>
2018-11-18 10:49:58 -0500

    Templates - Indentation and Verification Tags

--- webApp/urls.py
+++ webApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^registar_terapeuta/$', views.registerTherapist, name='register_therapist'),
+	url(r'^registrar_terapeuta/$', views.registerTherapist, name='register_therapist'),
 	url(r'^consultar_paciente/$', views.searchPatient, name='search_patient'),
 ]
--- webApp/views.py
+++ webApp/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
+from bokeh.plotting import figure, output_file, show 
+from bokeh.resources import CDN
+from bokeh.embed import components
+
 # Create your views here.
 def index(request):
-	return render(request, 'webApp/index.html')
+	x= [i for i in range(10)] #[0,1,2,3,4,5,6,7,8,9]
+	x2= [i**2 for i in range(10)]
+	x3= [i**3 for i in range(10)]
+	title_ = "y = f(x) = x^2"
+
+	plot = figure(title= title_,  x_axis_label= 'Indep.', y_axis_label= 'Dep.', plot_width =600,  plot_height =400)
+
+	plot.line(x, x2, legend= 'x^2', line_width = 2)
+	plot.line(x, x3, legend= 'x^3', line_width = 2)
+    	#Store components 
+	script, div = components(plot,CDN)
+
+	return render(request, 'webApp/index.html', {'script_plot' : script , 'div_plot' : div} )
+
 
 def registerTherapist(request):
 	return render(request, 'webApp/registroterapeuta.html')
